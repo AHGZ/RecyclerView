6e9f8c04c47e39ed49f51bf385a56fc5e5abc26e
18911005030@163.com <18911005030@163.com>
2017-08-31 20:06:46 +0800

    每个item增添CheckBox，增加全选，反选，单选功能

--- app/src/main/java/com/hgz/test/recyclerview/MainActivity.java
+++ app/src/main/java/com/hgz/test/recyclerview/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
     private GridLayoutManager gridLayoutManager;
+    private MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +25,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         Button button = (Button) findViewById(R.id.button);
-
+        Button btnSelectedAll = (Button) findViewById(R.id.btnSelectedAll);
+        Button btnReverseSelected = (Button) findViewById(R.id.btnReverseSelected);
         //RecyclerView的展示样式
         linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
@@ -42,10 +44,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        final MyAdapter myAdapter = new MyAdapter();
+        myAdapter = new MyAdapter();
         recyclerView.setAdapter(myAdapter);
         //在条目之间增添横线
-        recyclerView.addItemDecoration(new MyDecoration(this,LinearLayoutManager.VERTICAL));
+        recyclerView.addItemDecoration(new MyDecoration(this, LinearLayoutManager.VERTICAL));
         //使用单击事件接口，完成添加条目的操作
         myAdapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
             @Override
@@ -61,19 +63,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
         button.setOnClickListener(this);
+        btnSelectedAll.setOnClickListener(this);
+        btnReverseSelected.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
-        //if和elseif是有先后顺序的,先判断范围小的,然后再判断范围大的,
-        //因为GridLayoutManager 是继承 LinearLayoutManager ,所以他本质上也是LinearLayoutManager,
-        //所以不能先判断是否是LinearLayoutManager (LinearLayoutManager范围大)
-        if (layoutManager instanceof GridLayoutManager){
-            recyclerView.setLayoutManager(linearLayoutManager);
-        }else if(layoutManager instanceof LinearLayoutManager){
-            recyclerView.setLayoutManager(gridLayoutManager);
+        switch (v.getId()) {
+            case R.id.button:
+                RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+                //if和elseif是有先后顺序的,先判断范围小的,然后再判断范围大的,
+                //因为GridLayoutManager 是继承 LinearLayoutManager ,所以他本质上也是LinearLayoutManager,
+                //所以不能先判断是否是LinearLayoutManager (LinearLayoutManager范围大)
+                if (layoutManager instanceof GridLayoutManager) {
+                    recyclerView.setLayoutManager(linearLayoutManager);
+                } else if (layoutManager instanceof LinearLayoutManager) {
+                    recyclerView.setLayoutManager(gridLayoutManager);
+                }
+                break;
+            //控制RecyclerView列表中的CheckBox的全选
+            case R.id.btnSelectedAll:
+                myAdapter.selectedAll();
+                break;
+            //控制RecyclerView列表中的CheckBox的反选
+            case R.id.btnReverseSelected:
+                myAdapter.reverseSelected();
+                break;
         }
+
     }
 
 }
--- app/src/main/java/com/hgz/test/recyclerview/adapter/MyAdapter.java
+++ app/src/main/java/com/hgz/test/recyclerview/adapter/MyAdapter.java
@@ -4,51 +4,64 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.hgz.test.recyclerview.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Administrator on 2017/8/30.
  */
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     private List<String> data;
+    HashMap<Integer, Boolean> isCheckedHashMap;
 
-    public MyAdapter(){
-       data= new ArrayList<String>();
-       for (int i=0;i<30;i++){
-           data.add("这是条目"+i);
-       }
+    public MyAdapter() {
+        isCheckedHashMap = new HashMap<>();
+        data = new ArrayList<String>();
+        for (int i = 0; i < 30; i++) {
+            data.add("这是条目" + i);
+            isCheckedHashMap.put(i,false);
+        }
     }
+
     private OnItemClickListener onItemClickListener;
     private OnItemLongClickListener onItemLongClickListener;
+
     //定义接口
     //单击事件的接口
-    public interface OnItemClickListener{
-        void OnItemClick(View v,int position);
+    public interface OnItemClickListener {
+        void OnItemClick(View v, int position);
     }
+
     //长按事件的接口
-    public interface  OnItemLongClickListener{
-        void OnItemLongClick(View v,int position);
+    public interface OnItemLongClickListener {
+        void OnItemLongClick(View v, int position);
     }
+
     //设置单击事件的接口
-    public void setOnItemClickListener(OnItemClickListener onItemClickListener){
-        this.onItemClickListener=onItemClickListener;
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
     }
+
     //设置长按事件的接口
-    public void setOnItemLongClickListener(OnItemLongClickListener onItemLongClickListener){
-        this.onItemLongClickListener=onItemLongClickListener;
+    public void setOnItemLongClickListener(OnItemLongClickListener onItemLongClickListener) {
+        this.onItemLongClickListener = onItemLongClickListener;
     }
+
     //创建布局和viewHolder
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //inflate的时候,需要传入parent和attachToRoot==false; 使用传入三个参数的方法
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view, parent, false);
         return new MyViewHolder(view);
     }
 
@@ -64,7 +77,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
             @Override
             public void onClick(View v) {
                 //暴露一个单击回调接口
-                if (onItemClickListener!=null) {
+                if (onItemClickListener != null) {
                     onItemClickListener.OnItemClick(v, position);
                 }
             }
@@ -72,12 +85,23 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
         holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                if (onItemLongClickListener!=null) {
+                if (onItemLongClickListener != null) {
                     onItemLongClickListener.OnItemLongClick(v, position);
                 }
                 return true;
             }
         });
+        holder.checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isCheckedHashMap.put(position,!isCheckedHashMap.get(position));
+                notifyDataSetChanged();
+
+                //调用单选方法
+                //singleSelected(position);
+            }
+        });
+        holder.checkBox.setChecked(isCheckedHashMap.get(position));
     }
 
     @Override
@@ -89,28 +113,74 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
 
         private final ImageView showImage;
         private final TextView showText;
+        private final CheckBox checkBox;
         View itemView;
+
         public MyViewHolder(View itemView) {
             super(itemView);
             showImage = (ImageView) itemView.findViewById(R.id.showImage);
             showText = (TextView) itemView.findViewById(R.id.showText);
-            this.itemView=itemView;
+            checkBox = (CheckBox) itemView.findViewById(R.id.checkBox);
+            this.itemView = itemView;
         }
     }
+
     //增加条目的方法
-    public void addItem(int position){
-        data.add(position+1,"这是新添加的条目");
-        notifyItemRangeChanged(position+1,getItemCount());
+    public void addItem(int position) {
+        data.add(position + 1, "这是新添加的条目");
+        notifyItemRangeChanged(position + 1, getItemCount());
     }
+
     //删除条目的方法
-    public void deleteItem(int position){
+    public void deleteItem(int position) {
         data.remove(position);
-        notifyItemRangeRemoved(position,getItemCount());
+        notifyItemRangeRemoved(position, getItemCount());
     }
+
     //修给条目的方法
-    public void updateItem(int position){
+    public void updateItem(int position) {
         data.remove(position);
-        data.add(position,"这是修改的条目");
+        data.add(position, "这是修改的条目");
         notifyItemChanged(position);
     }
+    //全选的方法
+    public void selectedAll(){
+        Set<Map.Entry<Integer, Boolean>> entries = isCheckedHashMap.entrySet();
+        //如果发现有没有选中的item,我就应该去全部选中,这个变量就应该设置成true,否则就是false
+        boolean shouldSelectedAll = false;
+
+        //这个for循环就是判断一下接下来要全部选中,还是全部不选中
+        for (Map.Entry<Integer, Boolean> entry:entries) {
+            Boolean value = entry.getValue();
+            //如果有没选中的,那就去全部选中 ,如果发现全都选中了那就全部不选中,
+            if (!value){
+                shouldSelectedAll=true;
+                break;
+            }
+        }
+        //如果shouldSelectedAll为true说明需要全部选中,
+        // 如果为false说明没有没有选中的,已经是是全部选中的状态,需要全部不选中
+        for (Map.Entry<Integer, Boolean> entry:entries) {
+            entry.setValue(shouldSelectedAll);
+        }
+        notifyDataSetChanged();
+    }
+    //反选
+    public void reverseSelected(){
+        Set<Map.Entry<Integer, Boolean>> entries = isCheckedHashMap.entrySet();
+        for (Map.Entry<Integer, Boolean> entry:entries){
+            entry.setValue(!entry.getValue());
+        }
+        notifyDataSetChanged();
+    }
+    //单选 点击checkBox的时候只选中当前的item,在checkBox的点击事件中调用
+    public void singleSelected(int position){
+        Set<Map.Entry<Integer, Boolean>> entries = isCheckedHashMap.entrySet();
+        for (Map.Entry<Integer, Boolean> entry:entries) {
+            entry.setValue(false);
+        }
+        isCheckedHashMap.put(position,true);
+        notifyDataSetChanged();
+    }
+
 }
